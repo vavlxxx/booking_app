c57f14f042c0c61a4040f3f66e2db2ba1d6a0c18
vavlxxx <vladimir.nefedov357@gmail.com>
2025-06-09 13:45:53 +0500

    app is created

---
+++ main.py
@@ -0,0 +1,14 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
