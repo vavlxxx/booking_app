b63fa21adb71070d4942b8a4fe0f1645f9a9a181
vavlxxx <vladimir.nefedov357@gmail.com>
2025-06-14 18:55:41 +0500

    handled sqlalchemy.exc.IntegrityError

--- src/repos/auth.py
+++ src/repos/auth.py
@@ -1,8 +1,17 @@
+from fastapi import HTTPException
+from sqlalchemy import select
 from src.repos.base import BaseRepository
-from src.schemas.auth import User
+from src.schemas.auth import User, UserAdd
 from src.models.users import UsersOrm
 
 
 class AuthRepository(BaseRepository):
     model = UsersOrm
     schema = User
+
+    async def add(self, data: UserAdd):
+        result = await self.get_one_or_none(email=data.email)
+        if result is not None:
+            raise HTTPException(status_code=400, detail="User with that email address already exists. Try another")
+        
+        return await super().add(data)
