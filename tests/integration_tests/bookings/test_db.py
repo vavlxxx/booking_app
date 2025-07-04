96291952fdcc7aafbd96fa3ef55206b1137bae86
vavlxxx <vladimir.nefedov357@gmail.com>
2025-07-04 16:48:10 +0500

    changed booking test to full crud operations cycle

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -3,7 +3,7 @@ from datetime import date, timedelta
 from src.schemas.bookings import BookingAdd
 
 
-async def test_add_booking(db):
+async def test_booking_crud(db):
     user = (await db.auth.get_all())[0]
     room = (await db.rooms.get_all())[0]
     booking_data = BookingAdd(
@@ -14,5 +14,23 @@ async def test_add_booking(db):
         price=room.discounted_price
     )
     
-    await db.bookings.add(booking_data)
-    await db.commit()
+    booking = await db.bookings.add(booking_data)
+    assert booking
+
+    booking = await db.bookings.get_one_or_none(id=booking.id)
+    assert booking
+    assert booking.room_id == room.id
+    assert booking.date_from == date.today()+timedelta(days=1)
+    assert booking.date_to == date.today()+timedelta(days=10)
+    assert booking.price == room.discounted_price
+
+    booking.price = 0
+    await db.bookings.edit(booking, id=booking.id)
+    booking = await db.bookings.get_one_or_none(id=booking.id)
+    assert booking.price == 0
+
+    await db.bookings.delete(id=booking.id)
+    booking = await db.bookings.get_one_or_none(id=booking.id)
+    assert booking is None
+
+    await db.rollback()
